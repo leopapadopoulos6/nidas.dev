c3a062655bc58d11869113dca83d9556213f9b1f
leopapadopoulos6 <leopapadopoulos6@gmail.com>
2024-01-30 16:23:43 -0500

    header

--- nidas.dev/src/components/structure/header.js
+++ nidas.dev/src/components/structure/header.js
@@ -1,11 +1,26 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Spacer, Text, IconButton } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
 import { ColorModeSwitcher } from '../../themes/ColorModeSwitcher';
+import config from '../../data/text/config';
 
-const Header = () => (
-  <Box p={4} color="black">
-    Leonidas Papadopoulos
-    <ColorModeSwitcher/>
-  </Box>
-);
+
+
+const Header = ({name}) => {
+    name = config.name
+    return (
+        <Flex>
+            <Box p='4'>
+                <Text>{name}</Text>
+            </Box>
+            <Spacer />
+            <Box p='4'> 
+                <ColorModeSwitcher />
+            </Box>
+            <Box p='4'>
+                <IconButton size='sm' icon={<AddIcon />} />
+            </Box >
+        </Flex>
+    );
+};
 
 export default Header;
---
+++ nidas.dev/src/data/text/config.js
@@ -0,0 +1,5 @@
+const config = {
+    name: 'Leonidas Papadopoulos'
+}
+
+export default config
