5770b8eeb058647452d85ae7f6feeca8b71c11b6
leopapadopoulos6 <leopapadopoulos6@gmail.com>
2024-01-31 18:02:01 -0500

    mailto:leo@nidas.dev

--- nidas.dev/src/data/text/config.js
+++ nidas.dev/src/data/text/config.js
@@ -1,7 +1,8 @@
 const config = {
     name: 'LEONIDAS',
     construction: '...........who goes there..?',
-    email_link: 'leo@nidas.dev',
+    email: 'leo@nidas.dev',
+    email_link: 'mailto:leo@nidas.dev',
     github: 'https://github.com/leopapadopoulos6',
     linkedin: 'https://www.linkedin.com/in/leopapadopoulos6/'
 }
